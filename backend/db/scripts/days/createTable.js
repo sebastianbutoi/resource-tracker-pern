cc010f8f991d1e63a1087488ed980c1fdac5d774
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-02-28 00:28:37 +0000

    Added function to create days table

---
+++ backend/db/scripts/days/createTable.js
@@ -0,0 +1,10 @@
+import query from "../index.js";
+
+const sqlString = `CREATE TABLE IF NOT EXISTS days (day_id SERIAL PRIMARY KEY, name TEXT, date TEXT );`;
+
+export async function createDaysTable() {
+  const resp = await query(sqlString);
+  return resp;
+}
+
+createDaysTable();
