0823228eb787e1971623cc367979cf2a97a0f801
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-02-28 00:16:35 +0000

    Added pg pool

---
+++ backend/db/config.js
@@ -0,0 +1,10 @@
+import pg from "pg";
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+export default function query(text, params) {
+  return pool.query(text, params);
+}
