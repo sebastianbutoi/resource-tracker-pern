8b8a5cb694dfbdf3c2d8599917eeb06b92e6f0d8
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-02-28 00:39:55 +0000

    Created route for reading all data

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { readDaysTable } from "./db/scripts/days/readTable.js";
+import { readNotesTable } from "./db/scripts/notes/readTable.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +13,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Resource Tracker App</h1>");
+});
+
+app.get("/alldata", async (req, res) => {
+  const daysTable = await readDaysTable();
+  const noteTable = await readNotesTable();
+  const data = {
+    daysTable: daysTable,
+    noteTable: noteTable,
+  };
+  res.status(200).json(data);
 });
 
 app.listen(PORT, () => {
